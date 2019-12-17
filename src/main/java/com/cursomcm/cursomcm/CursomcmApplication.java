475130a1e8be2d75e25fb317941c78b316bc315f
niltonpc <niltonpc.snt@gmail.com>
2019-12-17 13:32:42 -0200

    Projeto Criado

---
+++ src/main/java/com/cursomcm/cursomcm/CursomcmApplication.java
@@ -0,0 +1,13 @@
+package com.cursomcm.cursomcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursomcm/cursomcm/CursomcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursomcm.cursomcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
